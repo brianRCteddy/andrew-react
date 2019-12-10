5b42c6009ae440053d02aca4269be137d1219ed1
Teddy <brianrcdteodoro@gmail.com>
2019-12-10 20:02:26 +0800

    added card component. fixed the cardlist accordingly. added image of each monster

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './CardList.css';
+import Card from '../card/Card';
 
 const CardList = (props) => {
-	console.log(props);
-	return <div className="cardList">{props.children}</div>;
+	return (
+		<div className="cardList">
+			{props.monsters.map((monster) => {
+				return <Card key={monster.id} monster={monster} />;
+			})}
+		</div>
+	);
 };
 
 export default CardList;
