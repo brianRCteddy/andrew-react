408b8f7321ca96ba103fd2da2cca46d26ef7eba9
Teddy <brianrcdteodoro@gmail.com>
2019-12-10 18:21:35 +0800

    setting state of monsters. mapping monster to create h1 list. adding key value

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			string: 'Hello Yema'
+			monsters: [
+				{
+					name: 'Dracula',
+					id: '2wq'
+				},
+				{
+					name: 'Frakenstein',
+					id: 'ffe'
+				},
+				{
+					name: 'Zombie',
+					id: 'ppo21'
+				}
+			]
 		};
 	}
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>{this.state.string}</p>
-					<button onClick={() => this.setState({ string: 'Hello Ted' })}>Click</button>
-				</header>
+				{this.state.monsters.map((monster) => {
+					return <h1 key={monster.id}>{monster.name}</h1>;
+				})}
 			</div>
 		);
 	}
