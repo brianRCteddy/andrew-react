57919cd015e4a90ff0fc89be23f2788cebd4bf92
Teddy <brianrcdteodoro@gmail.com>
2019-12-11 10:20:25 +0800

    final css design

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
 		);
 		return (
 			<div className="App">
+				<h1>Monster Rolodex</h1>
 				<SearchBar placeholder="Search here..." handleChange={this.handleChangeInput} />
 				<CardList monsters={filteredMonsters} />
 			</div>
