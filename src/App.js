bad212cc5b15fe107cbd38fe4fb8b334079fbeb8
Teddy <brianrcdteodoro@gmail.com>
2019-12-11 12:37:57 +0800

    setstate not fixed version - deleyed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import CardList from './components/card-list/CardList';
-import SearchBar from './components/searchbar/SearchBar';
 
 class App extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 		this.state = {
-			monsters: [],
-			searchField: ''
+			value: 69
 		};
 	}
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
-			response.json().then((users) => this.setState({ monsters: users }))
-		);
-	}
-
-	handleChangeInput = (event) => {
-		this.setState({ searchField: event.target.value });
+	handleChange = () => {
+		this.setState({ value: this.state.value + 1 });
+		console.log(this.state);
 	};
 
 	render() {
-		const { monsters, searchField } = this.state;
-		const filteredMonsters = monsters.filter((monster) =>
-			monster.name.toLowerCase().includes(searchField.toLowerCase())
-		);
 		return (
 			<div className="App">
-				<h1>Monster Rolodex</h1>
-				<SearchBar placeholder="Search here..." handleChange={this.handleChangeInput} />
-				<CardList monsters={filteredMonsters} />
+				<h1>React</h1>
+				<p>{this.state.value}</p>
+				<button onClick={this.handleChange}>Click</button>
 			</div>
 		);
 	}
