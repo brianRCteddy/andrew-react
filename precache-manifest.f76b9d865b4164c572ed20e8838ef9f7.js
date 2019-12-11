998564e0baed256fe7996788fad0e6653d361313
Teddy <brianrcdteodoro@gmail.com>
2019-12-11 10:35:07 +0800

    Updates

---
+++ precache-manifest.f76b9d865b4164c572ed20e8838ef9f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87d5b299a2a799a78863ba604a8042d",
+    "url": "/andrew-react/index.html"
+  },
+  {
+    "revision": "459a4701e134b1d5e150",
+    "url": "/andrew-react/static/css/main.df37ad64.chunk.css"
+  },
+  {
+    "revision": "c94a69cc1b8a1c166178",
+    "url": "/andrew-react/static/js/2.c749366e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/andrew-react/static/js/2.c749366e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "459a4701e134b1d5e150",
+    "url": "/andrew-react/static/js/main.11cbbb29.chunk.js"
+  },
+  {
+    "revision": "0cd033810c8867bb152d",
+    "url": "/andrew-react/static/js/runtime-main.36944bc5.js"
+  }
+]);
