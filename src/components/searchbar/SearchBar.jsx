525c474a8d26815c9125da6e9bd73df5cc735b6f
Teddy <brianrcdteodoro@gmail.com>
2019-12-11 09:07:04 +0800

    fixed searchbox. toLowerCase. destructuring

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -4,7 +4,13 @@ export default class SearchBar extends Component {
 	render() {
 		return (
 			<div>
-				<input type="text" />
+				<input
+					type="search"
+					placeholder="Search here..."
+					onChange={(e) => {
+						this.setState({ searchField: e.target.value });
+					}}
+				/>
 			</div>
 		);
 	}
